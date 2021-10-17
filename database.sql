474b541e554324ca2ce6eab93dd1c8c7e18df716
agoenks29D <agungmasda29@gmail.com>
2021-10-17 13:29:08 +0700

    backup database

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Okt 2021 pada 07.53
+-- Waktu pembuatan: 17 Okt 2021 pada 08.28
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 7.3.31
 
@@ -20,6 +20,20 @@ SET time_zone = "+00:00";
 --
 -- Database: `absensi-guru`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_kehadiran`
+--
+
+CREATE TABLE `data_kehadiran` (
+  `id` int(4) NOT NULL,
+  `user_id` int(2) NOT NULL,
+  `jam-masuk` time DEFAULT NULL,
+  `jam-keluar` time DEFAULT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -66,15 +80,16 @@ CREATE TABLE `mata_pelajaran` (
 
 CREATE TABLE `user` (
   `id` int(2) NOT NULL,
+  `nik` varchar(20) DEFAULT NULL,
   `role` enum('admin','guru') NOT NULL,
   `email` varchar(40) NOT NULL,
-  `username` varchar(50) NOT NULL,
+  `username` varchar(50) DEFAULT NULL,
   `password` varchar(40) NOT NULL,
   `full_name` varchar(40) NOT NULL,
   `birthday` date DEFAULT NULL,
   `religion` varchar(10) DEFAULT NULL,
-  `jurusan` int(2) DEFAULT NULL,
-  `guru_mapel` int(2) DEFAULT NULL,
+  `jurusan` varchar(20) DEFAULT NULL,
+  `guru_mapel` varchar(20) DEFAULT NULL,
   `photo` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -82,12 +97,18 @@ CREATE TABLE `user` (
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `role`, `email`, `username`, `password`, `full_name`, `birthday`, `religion`, `jurusan`, `guru_mapel`, `photo`) VALUES
-(1, 'admin', 'agungmasda29@gmail.com', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Administrator', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `user` (`id`, `nik`, `role`, `email`, `username`, `password`, `full_name`, `birthday`, `religion`, `jurusan`, `guru_mapel`, `photo`) VALUES
+(1, NULL, 'admin', 'agungmasda29@gmail.com', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Administrator', NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `data_kehadiran`
+--
+ALTER TABLE `data_kehadiran`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `email_confirm`
@@ -104,6 +125,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `data_kehadiran`
+--
+ALTER TABLE `data_kehadiran`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `email_confirm`
